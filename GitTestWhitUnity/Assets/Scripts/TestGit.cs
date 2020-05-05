513a08783290424a469e451cf7a12a3bef868efd
superzhangyj <64723401+superzhangyj@users.noreply.github.com>
2020-05-05 15:12:34 +0800

    Update TestGit.cs

--- GitTestWhitUnity/Assets/Scripts/TestGit.cs
+++ GitTestWhitUnity/Assets/Scripts/TestGit.cs
@@ -7,7 +7,7 @@ public class TestGit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //debug
     }
 
     // Update is called once per frame
